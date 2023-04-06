7df324e2dc1255f72a62f38c56983aeab2d6b8be
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-04-06 12:23:34 -0500

    added email and password length validation

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from dotenv import load_dotenv
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 import os
 
 load_dotenv()
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
 
 
